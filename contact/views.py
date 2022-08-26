31dd7ae5bede3410ea77d8d109c28ead795e9678
Artemio <artemiolucero@gmail.com>
2022-08-26 15:57:48 -0300

    Added contact view

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,16 @@
+from unicodedata import name
 from django.shortcuts import render
+from .models import Contact
+
 
 def index(request):
-    return render(request, 'contact/index.html', {})
+    req_search = request.GET.get('search', '')
+
+    if req_search:
+        contacts = Contact.objects.filter(name__contains=req_search)
+    else:
+        contacts = Contact.objects.all()
+
+    context = {'contacts': contacts}
+
+    return render(request, 'contact/index.html', context)
